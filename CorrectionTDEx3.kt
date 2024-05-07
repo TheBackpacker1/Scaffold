5d496437b3f39a8a7a9d165413eb68f096584131
mohamed louati <mohamedlouati002@gmail.com>
2024-05-07 23:22:56 +0100

    First commit Tp2

--- CorrectionTDEx3.kt
+++
@@ -1,8 +0,0 @@
-
-data class User(val name:String,val age:Int)
-fun main()
-{
-
-val user1=User("user1",20)
-println(user1)
-}
---
+++ RoadlessMobileApp/app/src/main/java/com/example/roadless/LandingPageComposable.kt
@@ -0,0 +1,27 @@
+package com.example.roadless
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.lazy.LazyColumn
+
+
+@Composable
+
+fun LandingPageComposable() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        LazyColumn{
+            item {
+     Image(
+    painter =)
+            }
+        }
+
+    }
+
+
+
+
+}
